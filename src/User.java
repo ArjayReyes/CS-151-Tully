7dbccf7af35c07826f7bc57b3c856d2635bc6754
Arjay <142987195+ArjayReyes@users.noreply.github.com>
2023-11-23 11:31:14 -0800

    Update User.java

--- src/User.java
+++ src/User.java
@@ -2,34 +2,69 @@ import java.util.ArrayList;
 //RAAAAAAAAAAAAAAAAAAh
 public class User {
 	private String name;
-	private String pass;
+	private String password;
 	private int libraryID;
 	private ArrayList<Book> booksBorrowed;//change to be generalized as books
+	private ArrayList<Book> booksWaitlisted;
 
 	public User() {
-		name = "Joe Biden";
-		pass = "sleepy";
-		libraryID = 1919191919;
-		booksBorrowed = new ArrayList<Book>();
+		this.name = "Joe Biden";
+		this.password = "sleepy";
+		this.libraryID = 1919191919;
+		this.booksBorrowed = new ArrayList<Book>();
+		this.booksWaitlisted = new ArrayList<Book>();
 	}
-	public User(String n, String p, int id, ArrayList<Book> ar) {
-		name = n;
-		pass = p;
-		libraryID = id;
-		booksBorrowed = ar;
+
+	public User(String n, String p, int id, ArrayList<Book> ar, ArrayList<Book> ar2) {
+		this.name = n;
+		this.password = p;
+		this.libraryID = id;
+		this.booksBorrowed = ar;
+		this.booksWaitlisted = ar2;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getName() {
 		return name;
 	}
-	public String getPassword() {
-		return pass;
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setLibraryID(int libraryID) {
+		this.libraryID = libraryID;
+	}
+
 	public int getId() {
 		return libraryID;
 	}
-	public ArrayList<Book> getBooks() {
+
+	public ArrayList<Book> getBooksBorrowed() {
 		return booksBorrowed;
 	}
+
+	public void setBooksBorrowed(ArrayList<Book> booksBorrowed) {
+		this.booksBorrowed = booksBorrowed;
+	}
+
+	public ArrayList<Book> getBooksWaitlisted() {
+		return booksWaitlisted;
+	}
+
+	public void setBooksWaitlisted(ArrayList<Book> booksWaitlisted) {
+		this.booksWaitlisted = booksWaitlisted;
+	}
+
+	// Added an equals method within Book class.
 	public boolean hasBook(String book) {//add a method in book to compare the contents
 		if (booksBorrowed.contains(book)) {
 			//System.out.println("yea");
@@ -54,9 +89,18 @@ public class User {
 		return false;
 
 	}
-	public String showBooks() {
-		String str = "Here are the books "+ name + " with Id "+libraryID+ " borrowed:\n";
+
+	public String showBooksBorrowed() {
+		String str = "Here are the books " + name + " with ID " + libraryID + " borrowed:\n";
 		for(Book s : booksBorrowed) {
+			str += s + "\n";
+		}
+		return str;
+	}
+
+	public String showBooksWaitlisted() {
+		String str = "Here are the books "+ name + " with ID "+ libraryID+ " waitlisted:\n";
+		for(Book s : booksWaitlisted) {
 			str += s + "\n";
 		}
 		return str;
@@ -65,8 +109,9 @@ public class User {
 	@Override
 	public String toString() {
 		return "\nUser: "+name+
-				"\nLibrary Id: "+libraryID+
-				"\nBooks Borrowed: "+booksBorrowed;
+				"\nLibrary ID: "+libraryID+
+				"\nBooks Borrowed: "+booksBorrowed+
+				"\nBooks waitlisted: "+booksWaitlisted;
 	}
 
 }
